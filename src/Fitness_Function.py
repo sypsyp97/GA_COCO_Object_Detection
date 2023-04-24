52338d54ea92ad35f61309421b661f8d3528667c
Yipeng Sun <sypsyp97@gmail.com>
2023-04-24 21:48:50 +0200

    update

--- src/Fitness_Function.py
+++ src/Fitness_Function.py
@@ -6,5 +6,6 @@ from math import pi
 def calculate_fitness(iou, inference_time):
     print("IOU:", iou)
     print("Inference Time:", inference_time)
-    fitness = (1 - np.arctan(inference_time / 500) / (pi / 2)) * iou
+    fitness = (1 - np.arctan(inference_time / 200) / (pi / 2)) * iou
+    print("Fitness:", fitness)
     return fitness
