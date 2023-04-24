1352555af8a17de119adaed7561c0b66039f74c0
Yipeng Sun <sypsyp97@gmail.com>
2023-04-24 21:50:42 +0200

    update

--- dataset/Dataset.py
+++ dataset/Dataset.py
@@ -60,8 +60,8 @@ for i in range(0, len(annot_paths)):
 )
 
 (x_val), (y_val) = (
-    np.asarray(images[int(len(images) * 0.8) : int(len(images) * 0.9)]),
-    np.asarray(targets[int(len(targets) * 0.8) : int(len(images) * 0.9)]),
+    np.asarray(images[int(len(images) * 0.8) :]),
+    np.asarray(targets[int(len(targets) * 0.8) :]),
 )
 
 train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(32).prefetch(tf.data.AUTOTUNE)
