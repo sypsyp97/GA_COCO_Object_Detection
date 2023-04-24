af70dbeaedc9f36ac3953da50533ab4d6c851d98
Yipeng Sun <sypsyp97@gmail.com>
2023-04-24 21:41:11 +0200

    update

--- src/Evolutionary_Algorithm.py
+++ src/Evolutionary_Algorithm.py
@@ -129,7 +129,7 @@ def select_models(train_ds,
     for i in range(population_array.shape[0]):
         model = create_model(population_array[i], num_classes=num_classes)
         model, _ = train_model(train_ds, val_ds, model=model, epochs=epochs)
-        iou = model_evaluation(model, test_ds)
+        iou = model_evaluation(model, test_ds) * 3.06
         try:
             tflite_model, tflite_name = convert_to_tflite(keras_model=model, generation=generation, i=i, time=time)
             edgetpu_name = compile_edgetpu(tflite_name)
